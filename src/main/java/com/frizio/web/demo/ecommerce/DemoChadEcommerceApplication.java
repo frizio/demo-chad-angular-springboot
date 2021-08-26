e1d0574814da2466989b2dd47fd91669241cf39f
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-08-26 09:55:31 +0200

    Refactor

--- src/main/java/com/frizio/web/demo/ecommerce/DemoChadEcommerceApplication.java
+++ src/main/java/com/frizio/web/demo/ecommerce/DemoChadEcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.frizio.web.demo.ecommerce.demochadecommerce;
+package com.frizio.web.demo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
