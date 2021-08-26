76fad1b3d723ffd00e920287023bd72ff3a90868
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-08-26 09:47:22 +0200

    first commit

---
+++ src/main/java/com/frizio/web/demo/ecommerce/demochadecommerce/DemoChadEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.frizio.web.demo.ecommerce.demochadecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoChadEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoChadEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frizio/web/demo/ecommerce/demochadecommerce/DemoChadEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frizio.web.demo.ecommerce.demochadecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoChadEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
