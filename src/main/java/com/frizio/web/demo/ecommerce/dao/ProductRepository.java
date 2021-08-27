9b8373146794142f8cb5b31d559d3a35e67bf9f3
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-08-27 17:25:43 +0200

    Change dev port to 4201 + crossorigin

--- src/main/java/com/frizio/web/demo/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/frizio/web/demo/ecommerce/dao/ProductRepository.java
@@ -5,7 +5,7 @@ import com.frizio.web.demo.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4201")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
