6484db6b82ae5955d1d0f3377b4fb4e7d1110e53
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-11-09 15:35:33 +0100

    Add NgbModule

--- ecommerce-frontend/src/app/app.module.ts
+++ ecommerce-frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
@@ -34,7 +35,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [
     ProductService
