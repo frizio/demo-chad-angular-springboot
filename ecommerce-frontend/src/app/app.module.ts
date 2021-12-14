daf64f95e874d766d49cac755d79dbb25a068670
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-14 18:28:18 +0100

    Add cart-status component

--- ecommerce-frontend/src/app/app.module.ts
+++ ecommerce-frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 
 // The routes start from most specific to generic!
@@ -30,7 +31,8 @@ const routes: Routes = [
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
