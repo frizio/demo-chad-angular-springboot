9919e2c803df5b8ee1365ef46d7e77d6bd147331
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-15 17:54:24 +0100

    Populate Country and State - Backend stack

--- src/main/java/com/frizio/web/demo/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/frizio/web/demo/ecommerce/config/MyDataRestConfig.java
@@ -7,8 +7,10 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
 
+import com.frizio.web.demo.ecommerce.entity.Country;
 import com.frizio.web.demo.ecommerce.entity.Product;
 import com.frizio.web.demo.ecommerce.entity.ProductCategory;
+import com.frizio.web.demo.ecommerce.entity.State;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -34,21 +36,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // Call an internal helper method
         this.exposeIds(config);
     }
+
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+      config.getExposureConfiguration()
+              .forDomainType(theClass)
+              .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+              .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+  }
 
 
     private void exposeIds(RepositoryRestConfiguration config) {
---
+++ src/main/java/com/frizio/web/demo/ecommerce/dao/StateRepository.java
@@ -0,0 +1,18 @@
+package com.frizio.web.demo.ecommerce.dao;
+
+import java.util.List;
+
+import com.frizio.web.demo.ecommerce.entity.State;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+
+}
