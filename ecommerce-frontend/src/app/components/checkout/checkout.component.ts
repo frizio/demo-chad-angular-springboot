970bb018c933ed5abd2ae1dad24f2bb6136048f4
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-15 13:12:59 +0100

    Define first form group (ts+html): customer

--- ecommerce-frontend/src/app/components/checkout/checkout.component.ts
+++ ecommerce-frontend/src/app/components/checkout/checkout.component.ts
@@ -15,6 +15,19 @@ export class CheckoutComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      })
+    });
+  }
+
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer').value);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
   }
 
 }
