8bbf83616b5e402ebcc21260a787ee83b9d093fe
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-15 13:02:17 +0100

    Define formgroup + inject formBuilder

--- ecommerce-frontend/src/app/components/checkout/checkout.component.ts
+++ ecommerce-frontend/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  checkoutFormGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
