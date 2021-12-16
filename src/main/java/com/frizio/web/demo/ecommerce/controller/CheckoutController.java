40f878e8fcecbb03004bfd5762b80390d3f4d4d7
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-16 18:37:30 +0100

    Save Order - Create Controller

---
+++ src/main/java/com/frizio/web/demo/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,30 @@
+package com.frizio.web.demo.ecommerce.controller;
+
+import com.frizio.web.demo.ecommerce.dao.service.CheckoutService;
+import com.frizio.web.demo.ecommerce.dto.Purchase;
+import com.frizio.web.demo.ecommerce.dto.PurchaseResponse;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:4201")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+
+}
