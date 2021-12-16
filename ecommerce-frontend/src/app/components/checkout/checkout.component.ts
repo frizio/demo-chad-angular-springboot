2a3692dce8eab62427d9f61c916283dd6f245a55
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-16 16:15:32 +0100

    Validation Rules - required + email

--- ecommerce-frontend/src/app/components/checkout/checkout.component.ts
+++ ecommerce-frontend/src/app/components/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { State } from './../../common/state';
 import { Country } from './../../common/country';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TheFormService } from './../../services/the-form.service';
 
 
@@ -31,9 +31,9 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName:  new FormControl( '', [Validators.required, Validators.minLength(2)] ),
+        lastName:   new FormControl( '', [Validators.required, Validators.minLength(2)] ),
+        email:      new FormControl( '', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')] )
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -87,8 +87,17 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+
+  // Getter 
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName()  { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email()     { return this.checkoutFormGroup.get('customer.email'); }
+
   onSubmit() {
     console.log("Handling the submit button");
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
     console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
