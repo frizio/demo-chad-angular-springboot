1e08961bb0b8c8238ed3eff1c89c17aa9983d693
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-17 17:36:27 +0100

    Handle browser refresh (persist on sessionStorage)

--- ecommerce-frontend/src/app/services/cart.service.ts
+++ ecommerce-frontend/src/app/services/cart.service.ts
@@ -11,8 +11,19 @@ export class CartService {
 
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
+
+  storage: Storage = sessionStorage;
+  // storage: Storage = localStorage;
   
-  constructor() { }
+  constructor() { 
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+    if (data != null) {
+      this.cartItems = data;
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+  }
 
 
   addToCart(theCartItem: CartItem) {
@@ -56,6 +67,8 @@ export class CartService {
     this.totalQuantity.next(totalQuantityValue);
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+    // persist cart data
+    this.persistCartItems();
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
@@ -86,6 +99,10 @@ export class CartService {
       this.cartItems.splice(itemIndex, 1);
       this.computeCartTotals();
     }
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
 }
