e30d3eb7b732e1218f49848460e71f11c9b26054
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-17 14:57:37 +0100

    fix default app config

--- ecommerce-frontend/src/app/config/the-app-config.ts
+++ ecommerce-frontend/src/app/config/the-app-config.ts
@@ -1,9 +1,9 @@
 export default {
-  oicd {
-    clientId: '',                                               // Public Identifier of the client app
-    issuer: '',                                                 // Issues of the token. URL when authorizing with Okta Authorization Server
-    redirectUri: '',                                            // Once user logs in... send them here
-    scopes: []                                                  // Scopes provide access to information about a user 
+  oidc: {
+    clientId: '********************',                           // Public Identifier of the client app
+    issuer: 'https://*********************/oauth2/default',     // Issues of the token. URL when authorizing with Okta Authorization Server
+    redirectUri: 'http://localhost:4201/login/callback',        // Once user logs in... send them here
+    scopes: ['openid', 'profile', 'email']                      // Scopes provide access to information about a user 
                                                                 // * 'openid'  -> required for authentication requests, 
                                                                 // * 'profile' -> user's first name, last name, phone, etc, 
                                                                 // * 'email'   -> user's email address
