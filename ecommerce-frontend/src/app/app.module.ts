fc25b9a2c2d94be3d1a5e0715f9b328a374a352b
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-17 16:29:22 +0100

    OKTA app.module configuration

--- ecommerce-frontend/src/app/app.module.ts
+++ ecommerce-frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -17,9 +17,32 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
 
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent
+} from '@okta/okta-angular';
+import myAppConfig from './config/the-app-config';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+
+const oktaConfig = Object.assign({
+  onAuthRequired: (injector) => {
+    const router = injector.get(Router);
+    // Redirect the user to your custom login page
+    router.navigate(['/login']);
+  }
+}, myAppConfig.oidc);
+
+const oktaAuth = new OktaAuth(oktaConfig);
+
 
 // The routes start from most specific to generic!
 const routes: Routes = [
+  //
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
+  //
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -51,10 +74,15 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
   providers: [
-    ProductService
+    ProductService,
+    { 
+      provide: OKTA_CONFIG, 
+      useValue: {oktaAuth} 
+    }
   ],
   bootstrap: [AppComponent]
 })
