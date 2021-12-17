8c55af433f97fcc12c3b7d0e673faa41e1f692fa
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2021-12-17 12:44:12 +0100

    Create OICD Client App on developer.okta.com

--- src/main/java/com/frizio/web/demo/ecommerce/DemoChadEcommerceApplication.java
+++ src/main/java/com/frizio/web/demo/ecommerce/DemoChadEcommerceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoChadEcommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoChadEcommerceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoChadEcommerceApplication.class, args);
+  }
 
 }
